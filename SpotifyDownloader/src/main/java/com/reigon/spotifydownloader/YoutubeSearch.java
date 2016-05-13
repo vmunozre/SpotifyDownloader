a348200b6458410ba90a52b5ea2cdccfdb95dd46
vmunozre <v.munozre@alumnos.urjc.es>
2016-05-13 12:31:24 +0200

    Procesador de Youtube creado y funcional :D

---
+++ SpotifyDownloader/src/main/java/com/reigon/spotifydownloader/YoutubeSearch.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.reigon.spotifydownloader;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Reiner
+ */
+public class YoutubeSearch {
+
+    List<Cancion> listaCanciones;
+    /**
+     * Global instance of the HTTP transport.
+     */
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    /**
+     * Global instance of the max number of videos we want returned (50 = upper
+     * limit per page).
+     */
+    private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+
+    /**
+     * Global instance of Youtube object to make all API requests.
+     */
+    private static YouTube youtube;
+
+    public YoutubeSearch() {
+        listaCanciones = new ArrayList<>();
+
+    }
+
+    public List<Cancion> process(List<Cancion> canciones) {
+
+        for (Cancion track : canciones) {
+            try {
+                /*
+       * The YouTube object is used to make all API requests. The last argument is required, but
+       * because we don't need anything initialized when the HttpRequest is initialized, we override
+       * the interface and provide a no-op function.
+                 */
+                youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                    public void initialize(HttpRequest request) throws IOException {
+                    }
+                }).setApplicationName("youtube-search-pruebas-spotify").build();
+
+                // Get query term from user.
+                String queryTerm = track.getQuery();
+
+                YouTube.Search.List search = youtube.search().list("id,snippet");
+                //No deberiamos poner el api key a pelo
+                String apiKey = "AIzaSyCkQfDbMO7A7Dtq062uB6xtRRmWypDIrAw";
+                search.setKey(apiKey);
+                search.setQ(queryTerm);
+
+                //Busqueda de solo de videos
+                search.setType("video");
+
+                search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+                search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+                SearchListResponse searchResponse = search.execute();
+
+                List<SearchResult> searchResultList = searchResponse.getItems();
+
+                if (searchResultList != null) {
+                    track.setUrl("https://www.youtube.com/watch?v=" + searchResultList.get(0).getId().getVideoId());
+                }
+            } catch (GoogleJsonResponseException e) {
+                System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                        + e.getDetails().getMessage());
+            } catch (IOException e) {
+                System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+
+        }
+
+        return canciones;
+    }
+}
